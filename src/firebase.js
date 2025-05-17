b645ff7155a115ecc558a6054c5251d94a563c49
Phi Nguyen <nph6969@gmail.com>
2025-05-16 22:55:43 -0700

    integrated Firestore

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_qwC6mA-RK-1hZ9C2uWu0ks_cc4HwkU4",
+  authDomain: "smiles-for-speech-2025.firebaseapp.com",
+  projectId: "smiles-for-speech-2025",
+  storageBucket: "smiles-for-speech-2025.firebasestorage.app",
+  messagingSenderId: "998951737438",
+  appId: "1:998951737438:web:d701da151995fcd8fa7ccf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
