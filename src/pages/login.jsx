d7de2b5bc1bbd0fbe79b323a665f5ded90505238
Phi Nguyen <nph6969@gmail.com>
2025-05-18 00:17:56 -0700

    fixed footer at bottom, remove navbar, add header-logo for login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,19 +1,34 @@
 import AuthForm from '../AuthForm';
 import '../styles/login.css';
+import headerLogo from '../assets/header-logo.png';
 
 export default function Login() {
   return (
-    <div className="login-container">
-      <div className="slideshow-background">
-        <div className="slide slide1"></div>
-        <div className="slide slide2"></div>
-        <div className="slide slide3"></div>
-        <div className="slide-overlay"></div>
-      </div>
-      <h1 className="login-title">Smiles for Speech</h1>
+    <div className="login-wrapper">
+      <div className="login-content">
+        <div className="slideshow-background">
+          <div className="slide slide1"></div>
+          <div className="slide slide2"></div>
+          <div className="slide slide3"></div>
+          <div className="slide-overlay"></div>
+        </div>
 
-      <div className="login-card">
-        <AuthForm />
+        <img
+              className='login-header-logo'
+              src={headerLogo}
+              alt="Smiles for Speech Logo"
+              style={{
+                height: '170px', // increased from 100px
+                maxWidth: '100%',
+                objectFit: 'contain',
+                marginBottom: '-3rem', // optional spacing below the logo
+                marginTop: '2rem'
+                
+              }}
+            />
+        <div className="login-card">
+          <AuthForm />
+        </div>
       </div>
     </div>
   );
